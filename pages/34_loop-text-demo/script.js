427c87812a1d6a355447fff679b178c2acc94abc
hsu win latt <lareina1467@gmail.com>
2026-03-22 17:53:01 +0700

    chore: kill the timeline

--- pages/34_loop-text-demo/script.js
+++ pages/34_loop-text-demo/script.js
@@ -1,29 +1,43 @@
 import gsap from "gsap";
 
 const words = document.querySelectorAll(".word-list p");
-const wordBoxWidth = document.querySelector(".word-list").offsetWidth;
-const containerWidth = document.querySelector(".mask-box").offsetWidth;
 
-const distance = containerWidth - wordBoxWidth;
-console.log(distance);
+let timeline;
 
-const timeline = gsap.timeline({
-  repeat: -1,
-  yoyo: true,
+function createAnimation() {
+  if (timeline) {
+    timeline.kill();
+  }
+
+  const wordBoxWidth = document.querySelector(".word-list").offsetWidth;
+  const containerWidth = document.querySelector(".mask-box").offsetWidth;
+
+  const distance = containerWidth - wordBoxWidth;
+
+  timeline = gsap.timeline({
+    repeat: -1,
+    yoyo: true,
+  });
+
+  timeline.fromTo(
+    words,
+    {
+      x: 0,
+      opacity: 0.1,
+    },
+    {
+      x: distance,
+      opacity: 1,
+      stagger: 0.05,
+      duration: 0.8,
+      ease: "back.out(1.8)",
+      delay: 0.5,
+    },
+  );
+}
+
+createAnimation();
+
+window.addEventListener("resize", () => {
+  createAnimation();
 });
-
-timeline.fromTo(
-  words,
-  {
-    x: 0,
-    opacity: 0.1,
-  },
-  {
-    x: distance,
-    opacity: 1,
-    stagger: 0.05,
-    duration: 0.8,
-    ease: "back.out(1.8)",
-    delay: 0.5,
-  },
-);
